ea5a066d4297c84caa45ada555e030a535a67490
tiwari-yukta <tiwariyukta55@gmail.com>
2026-03-06 21:42:36 +0530

    Remove exposed Firebase config from categories.ts

--- src/constants/categories.ts
+++ src/constants/categories.ts
@@ -6,29 +6,3 @@ export const PRODUCT_CATEGORIES: { value: string; label: string }[] = [
   { value: 'chocolates', label: 'Chocolates' },
   { value: 'sandwiches', label: 'Sandwiches' },
 ];
-
-// Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDCy-BrYyc7is5cyqEmeEP_J1DE5DnPzQ0",
-//   authDomain: "cafe-and-brew-dff10.firebaseapp.com",
-//   projectId: "cafe-and-brew-dff10",
-//   storageBucket: "cafe-and-brew-dff10.firebasestorage.app",
-//   messagingSenderId: "577897035432",
-//   appId: "1:577897035432:web:13eefe3265dbcd6c4cf49d",
-//   measurementId: "G-P7PJ1V9LM8"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-
-
-
-//npm install firebase
